9bda4c73e4c8ee246744d5abf39d0a6324718a8f
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-09-09 22:47:58 +0300

    adding styles and initializing board with letters

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+let map = Array();
+
+function initMap() {
+    map = [
+        //y0   y1  y2   y3   y4   y5    y6   y7
+        ["R", "P", " ", " ", " ", " ", "p", "r"],//x=0
+        ["N", "P", " ", " ", " ", " ", "p", "n"],//x=1
+        ["B", "P", " ", " ", " ", " ", "p", "b"],//x=2
+        ["Q", "P", " ", " ", " ", " ", "p", "q"],//x=3
+        ["K", "P", " ", " ", " ", " ", "p", "k"],//x=4
+        ["B", "P", " ", " ", " ", " ", "p", "b"],//x=5
+        ["N", "P", " ", " ", " ", " ", "p", "n"],//x=6
+        ["R", "P", " ", " ", " ", " ", "p", "r"] //x=7
+    ];
+}
+
 
 
 
 function showMap() {
     let html;
-    html = "<table border='1' cellpadding='2' cellspacing='0'    >"
+    html = "<table border='1' cellpadding='2' cellspacing='0'>"
     for (let y = 7; y >= 0; y--) {
         html += '<tr>'
         for (let x = 0; x <= 7; x++) {
-            html += "<td style=height:50px;width:50px></td>"
+            color = (x + y) % 2 ? 'white' : 'lightgreen';
+            html += "<td style=text-align:center;height:50px;width:50px;background-color:" + color +
+                ">";
+            html += map[x][y]
+            html += "</td >"
         }
         html += '</tr>'
     }
     document.getElementById('board').innerHTML = html;
 }
-showMap()
+
+initMap();
+showMap();
 
